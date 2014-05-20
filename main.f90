3131ae2fdb139a65a39d616c7ba703d336a79d2c
Tetsuo Koyama <tkoyama010@gmail.com>
2014-05-20 23:32:09 +0900

    read matrix-market

--- main.f90
+++ main.f90
@@ -1,4 +1,17 @@
 program main
     implicit none
-    print *, "Hello World"
+    integer*4 i,LineSize,ColumSize,DataSize,Line,Colum
+    real*8 Matrix,Data
+    allocatable Matrix(:,:)
+    read(*,*)
+    read(*,*)
+    read(*,*)LineSize,ColumSize,DataSize
+    allocate(Matrix(LineSize,ColumSize))
+    Matrix=0.0d0
+    do i=1,DataSize
+      read(*,*)Line,Colum,Data
+      Matrix(Line,Colum)=Data
+    enddo
+    print *, Matrix
+    deallocate(Matrix)
 end program
